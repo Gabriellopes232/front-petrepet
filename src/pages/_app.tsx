2453f8263523b76243ae2847d269e41933bf77b1
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-02-09 15:50:53 -0300

    refactor: structure app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+
+type NextAppProp = AppProps & {
+    Component: AppProps['Component'] & { requireAuth: boolean };
+};
+
+const App = ({ Component, pageProps }: NextAppProp): JSX.Element => {
+    return (
+        <Component {...pageProps} />
+    );
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export default function Home() {
+    //TODO Page redirect to Login
+    return <div className="bg-slate-900 w-screen" />
+}
