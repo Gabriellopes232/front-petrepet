62cec74d79c6b7dc7627d1bdaaf03160b6b29cd4
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-02-09 16:05:34 -0300

    fix: style format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,11 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 type NextAppProp = AppProps & {
-    Component: AppProps['Component'] & { requireAuth: boolean };
+  Component: AppProps['Component'] & { requireAuth: boolean };
 };
 
 const App = ({ Component, pageProps }: NextAppProp): JSX.Element => {
-    return (
-        <Component {...pageProps} />
-    );
+  return <Component {...pageProps} />;
 };
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
 export default function Home() {
-    //TODO Page redirect to Login
-    return <div className="bg-slate-900 w-screen" />
+  //TODO Page redirect to Login
+  return <div className="bg-slate-900 w-screen" />;
 }
